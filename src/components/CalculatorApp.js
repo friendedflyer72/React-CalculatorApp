219dd0f86fae5d3abb686ebf0311424f9d95d1cc
Samiran Das <samirandascr@gmail.com>
2024-05-07 19:33:58 +1000

    Added Fuctionality

--- src/components/CalculatorApp.js
+++ src/components/CalculatorApp.js
@@ -4,7 +4,7 @@ import { Typography, TextField, Box, Grid, Button } from "@mui/material";
 import { green } from "@mui/material/colors";
 
 function CalculatorApp() {
- const[buttonBaseClasses, setButton] = useState();
+ const[value, setValue] = useState('');
   return (
     <>
       <Typography variant="h4" color={"black"} margin={2}>
@@ -14,6 +14,7 @@ function CalculatorApp() {
         <div>
           <TextField
             placeholder="Enter Number"
+            value={value}
             variant="outlined"
             fullWidth
             sx={{
@@ -26,7 +27,7 @@ function CalculatorApp() {
                 "& .MuiOutlinedInput-notchedOutline": {
                   borderColor: "#000",
                   borderWidth: "1px",
-                  padding:'30px',
+                  padding:'20px',
                 },
               },
               // Class for the label of the input field
@@ -39,92 +40,92 @@ function CalculatorApp() {
           <Box sx={{ flexGrow: 1, mt: 2 }}>
             <Grid container>
             <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={[{p: 1.5, backgroundColor: 'green', fontSize: '16px' , fontWeight:'700'}]}>
+                <Button variant="contained" fullWidth sx={[{p: 1.5, backgroundColor: 'green', fontSize: '16px' , fontWeight:'700'}]} onClick={e =>setValue("")}>
                   AC
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={[{p: 1.5, backgroundColor: 'red', fontSize: '16px' , fontWeight:'700'}]}>
+                <Button variant="contained" fullWidth sx={[{p: 1.5, backgroundColor: 'red', fontSize: '16px' , fontWeight:'700'}]} onClick={e =>setValue(value.slice(0, -1))}>
                   DEL
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="." fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   .
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="/" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   /
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="7" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   7
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="8" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   8
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="9" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   9
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="*" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   X
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="4" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   4
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="5" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   5
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="6" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   6
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="+" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   +
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="1" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   1
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="2" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   2
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="3" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   3
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="-" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   -
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="00" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   00
                 </Button>
               </Grid>
               <Grid p xs={3}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="0" fullWidth sx={{p: 1.5, fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(value + e.target.value)}>
                   0
                 </Button>
               </Grid>
@@ -134,7 +135,7 @@ function CalculatorApp() {
                 </Button>
               </Grid> */}
               <Grid p xs={6}>
-                <Button variant="contained" fullWidth sx={{p: 1.5, backgroundColor: '#06d6a0', fontSize: '16px' , fontWeight:'700'}}>
+                <Button variant="contained" value="=" fullWidth sx={{p: 1.5, backgroundColor: '#06d6a0', fontSize: '16px' , fontWeight:'700'}} onClick={e => setValue(eval(value))}>
                   =
                 </Button>
               </Grid>
